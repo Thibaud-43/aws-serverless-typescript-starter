69e024e64fd812818c8892605fb367f15f3bb5d1
Rouchon <rouchon@MacBook-Air-de-Rouchon.local>
2022-07-15 13:55:57 +0200

    a starter for an aws serverless projec

---
+++ src/hello.test.ts
@@ -0,0 +1,5 @@
+import { hello } from './hello';
+
+test('Should return hello', () => {
+  expect(hello()).toBe('hello');
+});
---
+++ src/hello.ts
@@ -0,0 +1,19 @@
+interface Response {
+  statusCode: number;
+  body: string;
+  headers: {
+    'Access-Control-Allow-Origin': string;
+  };
+}
+export const hello = (): string => 'hello';
+const handler = async (): Promise<Response> => {
+  const response = {
+    statusCode: 200,
+    body: hello(),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  };
+  return response;
+};
+export default handler;
